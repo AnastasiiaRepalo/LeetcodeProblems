b35b4010acb0e90cb60926324c3c2c593bde247e
anastasiia <repalo.a@gmail.com>
2022-11-14 19:27:44 +0000

    added 2379, 1876

---
+++ arrays/ longest_nice_substring_1763/main.py
@@ -0,0 +1,13 @@
+from solution import Solution
+
+
+def main():
+    s = Solution()
+    string = "aDdaaA"
+
+    res = s.longestNiceSubstring(string)
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
